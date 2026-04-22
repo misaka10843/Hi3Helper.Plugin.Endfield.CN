5b2fbfbda27511ef9b2e4e0769002eba7900cca1
misaka10843 <misaka10843@outlook.jp>
2026-04-22 20:33:46 +0800

    feat: add arknights plugins

---
+++ Hi3Helper.Plugin.Arknights/Management/PresetConfig/ArknightsCnPresetConfig.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.InteropServices.Marshalling;
+using System.Threading;
+using System.Threading.Tasks;
+using Hi3Helper.Hypergryph.Core.Management;
+using Hi3Helper.Hypergryph.Core.Management.Api;
+using Hi3Helper.Plugin.Core.Management;
+using Hi3Helper.Plugin.Core.Management.Api;
+using Hi3Helper.Plugin.Core.Management.PresetConfig;
+
+namespace Hi3Helper.Plugin.Arknights.Management.PresetConfig;
+
+[GeneratedComClass]
+public partial class ArknightsCnPresetConfig : PluginPresetConfigBase
+{
+    private const string ExEcutableName = "Arknights.exe";
+ 
+    private const string ExApiUrl = "https://launcher.hypergryph.com/api/proxy/batch_proxy";
+    private const string ExWebApiUrl = "https://launcher.hypergryph.com/api/proxy/web/batch_proxy";
+    private const string ExAppCode = "GzD1CpaWgmSq1wew";
+    private const string ExLauncherAppCode = "abYeZZ16BPluCFyT";
+    private const string ExChannel = "1";
+    private const string ExSubChannel = "1";
+    private const string ExSeq = "5";
+
+    [field: AllowNull] [field: MaybeNull] public override string GameName => field ??= "Arknights";
+    [field: AllowNull] [field: MaybeNull] public override string GameExecutableName => field ??= ExEcutableName;
+
+    public override string GameAppDataPath
+    {
+        get
+        {
+            string? gamePath = null;
+            GameManager?.GetGamePath(out gamePath);
+            if (!string.IsNullOrEmpty(gamePath)) return Path.Combine(gamePath, "Arknights_Data");
+            return string.Empty;
+        }
+    }
+
+    [field: AllowNull] [field: MaybeNull] public override string GameLogFileName => field ??= null!;
+
+    [field: AllowNull] [field: MaybeNull] public override string GameVendorName => field ??= "Hypergryph";
+    [field: AllowNull] [field: MaybeNull] public override string GameRegistryKeyName => field ??= "Arknights";
+    [field: AllowNull] [field: MaybeNull] public override string ProfileName => field ??= "ArknightsCn";
+
+    [field: AllowNull]
+    [field: MaybeNull]
+    public override string ZoneDescription => field ??= "《明日方舟》是一款魔物主题的策略手游。";
+
+    [field: AllowNull] [field: MaybeNull] public override string ZoneName => field ??= "Mainland China";
+    [field: AllowNull] [field: MaybeNull] public override string ZoneFullName => field ??= "明日方舟 (中国大陆)";
+    [field: AllowNull] [field: MaybeNull] public override string ZoneLogoUrl => field ??= "";
+    [field: AllowNull] [field: MaybeNull] public override string ZonePosterUrl => field ??= "";
+
+    [field: AllowNull]
+    [field: MaybeNull]
+    public override string ZoneHomePageUrl => field ??= "https://ak.hypergryph.com/";
+
+    public override GameReleaseChannel ReleaseChannel => GameReleaseChannel.Public;
+
+    [field: AllowNull] [field: MaybeNull] public override string GameMainLanguage => field ??= "zh-CN";
+
+    [field: AllowNull]
+    [field: MaybeNull]
+    public override string LauncherGameDirectoryName => field ??= "Arknights Game";
+
+    [field: AllowNull] [field: MaybeNull] public override List<string> SupportedLanguages => field ??= ["Chinese"];
+
+    public override ILauncherApiMedia? LauncherApiMedia
+    {
+        get => field ??= new HgLauncherApiMedia(ExWebApiUrl, ExAppCode, ExChannel, ExSubChannel, ExSeq);
+        set;
+    }
+
+    public override ILauncherApiNews? LauncherApiNews
+    {
+        get => field ??= new HgLauncherApiNews(ExWebApiUrl, ExAppCode, ExChannel, ExSubChannel, ExSeq);
+        set;
+    }
+
+    public override IGameManager? GameManager
+    {
+        get => field ??= new HgGameManager(ExEcutableName, ExApiUrl, ExWebApiUrl, ExAppCode, ExLauncherAppCode,
+            ExChannel, ExSubChannel, ExSeq);
+        set;
+    }
+
+    public override IGameInstaller? GameInstaller
+    {
+        get => field ??= new HgGameInstaller(GameManager!);
+        set;
+    }
+
+    protected override Task<int> InitAsync(CancellationToken token)
+    {
+        return Task.FromResult(0);
+    }
+}
---
+++ Hi3Helper.Plugin.Arknights/Plugin.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+using Hi3Helper.Plugin.Arknights.Management.PresetConfig;
+using Hi3Helper.Plugin.Arknights.Utils;
+using Hi3Helper.Plugin.Core;
+using Hi3Helper.Plugin.Core.Management.PresetConfig;
+using Hi3Helper.Plugin.Core.Update;
+using Hi3Helper.Plugin.Core.Utility;
+using Microsoft.Extensions.Logging;
+
+namespace Hi3Helper.Plugin.Arknights;
+
+[GeneratedComClass]
+public partial class ArknightsPlugin : PluginBase
+{
+    private static readonly IPluginPresetConfig[] PresetConfigInstances =
+    [
+        new ArknightsCnPresetConfig(),
+        new ArknightsBiliPresetConfig()
+    ];
+
+    private static DateTime _pluginCreationDate = new(2026, 02, 13, 00, 00, 0, DateTimeKind.Utc);
+
+    public override void GetPluginName(out string result)
+    {
+        result = "Arknights Plugin";
+    }
+
+    public override void GetPluginDescription(out string result)
+    {
+        result = "A plugin for Arknights in Collapse Launcher";
+    }
+
+    public override void GetPluginAuthor(out string result)
+    {
+        result = "misaka10843";
+    }
+
+    public override unsafe void GetPluginCreationDate(out DateTime* result)
+    {
+        result = _pluginCreationDate.AsPointer();
+    }
+
+    public override void GetPresetConfigCount(out int count)
+    {
+        count = PresetConfigInstances.Length;
+    }
+
+    public override void GetPresetConfig(int index, out IPluginPresetConfig presetConfig)
+    {
+        SharedStatic.InstanceLogger.LogInformation("[Arknights] Starting execution...");
+        if (index < 0 || index >= PresetConfigInstances.Length)
+        {
+            presetConfig = null!;
+            return;
+        }
+
+        presetConfig = PresetConfigInstances[index];
+    }
+
+    public override void GetPluginSelfUpdater(out IPluginSelfUpdate selfUpdate)
+    {
+        selfUpdate = new SelfUpdate();
+    }
+
+    public override void GetPluginAppIconUrl(out string result)
+    {
+        result = Convert.ToBase64String(ArknightsImageData.ArknightsAppIconData);
+    }
+
+    public override void GetNotificationPosterUrl(out string result)
+    {
+        result = Convert.ToBase64String(ArknightsImageData.ArknightsPosterData);
+    }
+}
